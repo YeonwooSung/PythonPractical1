230b7a3bf69024529aae1c3c66c098b38d99a4b1
Yeonwoo Sung <30489717+YeonwooSung@users.noreply.github.com>
2018-03-01 01:31:25 +0900

    Add files via upload

---
+++ easyextension.py
@@ -0,0 +1,82 @@
+from twistedint import *
+
+#Function for the first Easy extension.
+def findMul1(n):
+    results = []
+    for i in range (2, n): #We should skip the 1, because the TwistedInt(1,1) is an illegal expression!
+        for j in range (0, i):
+            ti = TwistedInt(j, i)
+            if (ti * ti) == TwistedInt(1, i):
+                results.append(ti)
+    return results
+
+#Function for the second Easy extension.
+def checkReverse(n, commandNum):
+    if commandNum == 1:
+        return checkReverseAdd(n)
+    elif commandNum == 2:
+        return checkReverseMultiply(n)
+    elif commandNum == 3:
+        return checkReverseAddAndMult(n)
+    elif commandNum == 4:
+        return checkReverseMultiplyAndMult(n)
+    elif commandNum == 5:
+        return checkReverseNested(n)
+    else:
+        raise ValueError("Wrong command number! The second parameter should be the value between 1 to 5")
+
+#Function to check if the condition "x+y == y+x" is true
+def checkReverseAdd(n):
+    for x in range (0, n-1):
+        for y in range (0, n-1):
+            ti1 = TwistedInt(x, n)
+            ti2 = TwistedInt(y, n)
+            if not ((ti1 + ti2) == (ti2 + ti1)):
+                return False
+    return True
+
+#Function to check if the condition "x*y == y*x" is true
+def checkReverseMultiply(n):
+    for x in range (0, n-1):
+        for y in range (0, n-1):
+            ti1 = TwistedInt(x, n)
+            ti2 = TwistedInt(y, n)
+            if not ((ti1 * ti2) == (ti2 * ti1)):
+                return False
+    return True
+
+#Function to check if the condition "(x+y)* z == x+(y*z)" is true
+def checkReverseAddAndMult(n):
+    for x in range (0, n-1):
+        for y in range (0, n-1):
+            for z in range (0, n-1):
+                ti1 = TwistedInt(x, n)
+                ti2 = TwistedInt(y, n)
+                ti3 = TwistedInt(z, n)
+                if not (((ti1 + ti2)*ti3) == (ti1+(ti2*ti3))):
+                    return False
+    return True
+
+#Function to check if the condition "(x*y)*z == x*(y*z)" is true
+def checkReverseMultiplyAndMult(n):
+    for x in range (0, n-1):
+        for y in range (0, n-1):
+            for z in range (0, n-1):
+                ti1 = TwistedInt(x, n)
+                ti2 = TwistedInt(y, n)
+                ti3 = TwistedInt(z, n)
+                if not (((ti1*ti2)*ti3) == (ti1*(ti2*ti3))):
+                    return False
+    return True
+
+#Function to check if the condition "(x+y)*z == (x*z)+(y*z)"
+def checkReverseNested(n):
+    for x in range (0, n-1):
+        for y in range (0, n-1):
+            for z in range (0, n-1):
+                ti1 = TwistedInt(x, n)
+                ti2 = TwistedInt(y, n)
+                ti3 = TwistedInt(z, n)
+                if not (((ti1+ti2)*ti3) == ((ti1*ti3) + (ti2*ti3))):
+                    return False
+    return True
---
+++ twistedint.py
@@ -0,0 +1,45 @@
+class TwistedInt:
+    def __init__(self, val, n):
+        if n < 0:
+            raise ValueError("The range of the value should be greater than 0.")
+        elif (n < val) and (0 > val):
+            raise ValueError("The value of the TwistedInt should be in range of (0 ~ n)")
+        else :
+            self.val = val
+            self.n = n
+
+    # Overwrite "print"
+    def __str__(self):
+        return "<" + str(self.val) + ":" + str(self.n) + ">"
+
+    # Define the operator +
+    def __add__(self, other):
+        if not isinstance(self, other.__class__):
+            raise TypeError("The type of the second argument is not the TwistedInt!")
+        elif self.n != other.n:
+            raise ValueError("The value n of the two objects should be same!")
+        else:
+            return TwistedInt((self.val + other.val) % self.n, self.n)
+
+    # Define the operator *
+    def __mul__(self, other):
+        if not isinstance(self, other.__class__):
+            raise TypeError("The type of the second argument is not the TwistedInt!")
+        elif self.n != other.n:
+            raise ValueError("The value n of the two objects should be same!")
+        else:
+            return TwistedInt((self.val + other.val + self.val * other.val) % self.n, self.n)
+
+    # Overwrite the function eq to define the operator ==
+    def __eq__(self, other):
+        if isinstance(self, other.__class__):
+            return self.val == other.val and self.n == other.n
+        return False
+
+    # Overwrite the function ne to define the operator !=
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    # Overwrite the repr function to represent the object with the format that we want.
+    def __repr__(self):
+        return str(self)
