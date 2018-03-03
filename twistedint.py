7f162edea5d0d5de9b67e01e7969a4c92dc30d02
Yeonwoo Sung <30489717+YeonwooSung@users.noreply.github.com>
2018-03-03 23:00:39 +0000

    Update twistedint.py

--- twistedint.py
+++ twistedint.py
@@ -19,7 +19,7 @@ class TwistedInt:
         >>> TwistedInt(1, 1)
         Traceback (most recent call last):
         ...
-        ValueError: The value of the TwistedInt should be in range of 0 ~ n-1
+        ValueError: The value should be in the range of 0 ~ n-1
 
         >>> TwistedInt(-2, -5)
         Traceback (most recent call last):
@@ -29,7 +29,7 @@ class TwistedInt:
         if n < 0:
             raise ValueError("The range of the value should be greater than 0.")
         elif (n <= val) or (0 > val):
-            raise ValueError("The value of the TwistedInt should be in range of 0 ~ n-1")
+            raise ValueError("The value should be in the range of 0 ~ n-1")
         else :
             self.val = val
             self.n = n
