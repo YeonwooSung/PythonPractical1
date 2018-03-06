e88ac0984a6241d4a7bd3bdcc21f94f4eecd7d02
Yeonwoo Sung <30489717+YeonwooSung@users.noreply.github.com>
2018-03-06 16:11:33 +0000

    Add files via upload

--- twistedintegers.py
+++ twistedintegers.py
@@ -9,6 +9,21 @@ class TwistedIntegers:
         Creates a new TwitedIntegers object.
 
         :param n: is the size of the twisted integers.
+
+        >>> TwistedIntegers(5)
+        [<0:5>, <1:5>, <2:5>, <3:5>, <4:5>]
+
+        >>> TwistedIntegers(10)
+        [<0:10>, <1:10>, <2:10>, <3:10>, <4:10>, <5:10>, <6:10>, <7:10>, <8:10>, <9:10>]
+
+        >>> TwistedIntegers(0)
+        []
+
+        >>> TwistedIntegers(-1)
+        []
+
+        >>> TwistedIntegers(-10)
+        []
         """
         self.size = n #Add a field called size, which is the size of Zn, which is a set of TwistedInts.
         self.list = [] #The list which cotains all twisted int objects in it
@@ -21,6 +36,21 @@ class TwistedIntegers:
         This returns the string of the list that contains n twisted ints in the twisted integers.
 
         :return: (self.list) contains n twisted int objects.
+
+        >>> str(TwistedIntegers(1))
+        '[<0:1>]'
+
+        >>> str(TwistedIntegers(0))
+        '[]'
+
+        >>> str(TwistedIntegers(-1))
+        '[]'
+
+        >>> str(TwistedIntegers(-10))
+        '[]'
+
+        >>> str(TwistedIntegers(5))
+        '[<0:5>, <1:5>, <2:5>, <3:5>, <4:5>]'
         """
         return str(self.list) #Return the string of the list
 
@@ -30,6 +60,21 @@ class TwistedIntegers:
         Similar with the str method.
 
         :return: (self.list) contains n twisted int objects.
+
+        >>> repr(TwistedIntegers(1))
+        '[<0:1>]'
+
+        >>> repr(TwistedIntegers(0))
+        '[]'
+
+        >>> repr(TwistedIntegers(-1))
+        '[]'
+
+        >>> repr(TwistedIntegers(-10))
+        '[]'
+
+        >>> repr(TwistedIntegers(5))
+        '[<0:5>, <1:5>, <2:5>, <3:5>, <4:5>]'
         """
         return str(self.list) #Return the string of the list
 
@@ -54,6 +99,18 @@ class TwistedIntegers:
 
         >>> TwistedIntegers(5).IntegerList()
         [<0:5>, <1:5>, <2:5>, <3:5>, <4:5>]
+
+        >>> TwistedIntegers(0).IntegerList()
+        []
+
+        >>> TwistedIntegers(-1).IntegerList()
+        []
+
+        >>> TwistedIntegers(-10).IntegerList()
+        []
+
+        >>> TwistedIntegers(10).IntegerList()
+        [<0:10>, <1:10>, <2:10>, <3:10>, <4:10>, <5:10>, <6:10>, <7:10>, <8:10>, <9:10>]
         """
         return self.list
 
@@ -68,11 +125,34 @@ class TwistedIntegers:
         >>> TwistedIntegers(5).addTwoTwistedInt(3, 4)
         <2:5>
 
+        >>> TwistedIntegers(10).addTwoTwistedInt(5, 6)
+        <1:10>
+
+        >>> TwistedIntegers(30).addTwoTwistedInt(5, 6)
+        <11:30>
+
+        >>> TwistedIntegers(50).addTwoTwistedInt(5, 6)
+        <11:30>
+
+        >>> TwistedIntegers(70).addTwoTwistedInt(5, 6)
+        <11:30>
+
+        >>> TwistedIntegers(90).addTwoTwistedInt(5, 6)
+        <11:30>
+
+        >>> TwistedIntegers(50).addTwoTwistedInt(5, 66)
+        <71:30>
+
         Errors:
         >>> TwistedIntegers(5).addTwoTwistedInt(5, 4)
         Traceback (most recent call last):
         ...
         TypeError: The value of the TwistedInt should be in the range of 0 ~ 4
+
+        >>> TwistedIntegers(2).addTwoTwistedInt(1, 2)
+        Traceback (most recent call last):
+        ...
+        TypeError: The value of the TwistedInt should be in the range of 0 ~ 1
         """
         if (not (0 < first < self.size)) or (not (0 < second < self.size)):#To check if the arguments are in the range
             message = "The value of the TwistedInt should be in the range of 0 ~ "
@@ -92,11 +172,22 @@ class TwistedIntegers:
         >>> TwistedIntegers(5).mulTwoTwistedInt(3, 4)
         <4:5>
 
+        >> TwistedIntgers(10).mulTwoTwistedInt(7, 9)
+        <9:10>
+
+        >>> TwistedIntegers(30).mulTwoTwistedInt(20, 25)
+        <5:30>
+
         Errors:
         >>> TwistedIntegers(5).addTwoTwistedInt(5, 4)
         Traceback (most recent call last):
         ...
         TypeError: The value of the TwistedInt should be in the range of 0 ~ 4
+
+        >>> TwistedIntegers(2).mulTwoTwistedInt(1, 2)
+        Traceback (most recent call last):
+        ...
+        TypeError: The value of the TwistedInt should be in the range of 0 ~ 1
         """
         if (not (0 < first < self.size)) or (not (0 < second < self.size)):#To check if the arguments are in the range
             message = "The value of the TwistedInt should be in the range of 0 ~ "
